e3a1752870f8ae9ac8cbcd98d4e1be1c4ad14724
Chris_Eteka_Dellcls <christopher.eteka.200986@unn.edu.ng>
2019-08-05 13:03:38 +0100

    Added a datasource config

--- src/main/java/com/chris/printmatcher/fingerprintmatcher/Configuration/AppConfig/AppConfig.java
+++ src/main/java/com/chris/printmatcher/fingerprintmatcher/Configuration/AppConfig/AppConfig.java
@@ -1,5 +1,8 @@
 package com.chris.printmatcher.fingerprintmatcher.Configuration.AppConfig;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,6 +13,8 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableSwagger2
@@ -35,5 +40,15 @@ public class AppConfig {
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
                 .contact(new Contact("KRIS E. I.", "http://no_website.com", "christopher.eteka.200986@unn.edu.ng"))
                 .build();
+    }
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
     }
 }
